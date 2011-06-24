aa24e95b5115140d2c25aafcb66d0786e210a2db
gigadot <gigadot@gmail.com>
2011-06-24 15:18:53 +0100

    Added more fields into thermo class

--- src/thermo.cpp
+++ src/thermo.cpp
@@ -16,6 +16,68 @@ const std::string IO::Thermo::getSpeciesName() const {
     return species_name_;
 }
 
+void IO::Thermo::setNote(std::string note) {
+    note_ = note;
+}
+
+std::string IO::Thermo::getNote() {
+    return note_;
+}
+
+void IO::Thermo::setPhase(std::string phase) {
+    phase_ = phase;
+}
+
+std::string IO::Thermo::getPhase() {
+    return phase_;
+}
+
+void IO::Thermo::setTLow(double T_low) {
+    T_low_ = T_low;
+}
+
+double IO::Thermo::getTLow() {
+    return T_low_;
+}
+
+void IO::Thermo::setTCommon(double T_common) {
+    T_common_ = T_common;
+}
+
+double IO::Thermo::getTCommon() {
+    return T_common_;
+}
+
+void IO::Thermo::setTHigh(double T_high) {
+    T_high_ = T_high;
+}
+
+double IO::Thermo::getTHigh() {
+    return T_high_;
+}
+
+void IO::Thermo::setElementsCounts(std::string elements_counts) {
+    elements_counts_ = elements_counts;
+}
+
+std::string IO::Thermo::getElementsCounts() {
+    return elements_counts_;
+}
+
+void IO::Thermo::setUpperTemperatureCoefficients(
+        double ah1, double ah2, double ah3,
+        double ah4, double ah5, double ah6,
+        double ah7) {
+
+}
+
+void IO::Thermo::setLowerTemperatureCoefficients(
+        double al1, double al2, double al3,
+        double al4, double al5,
+        double al6, double al7) {
+
+}
+
 namespace IO {
 
     std::ostream& operator<<(std::ostream& output, const Thermo& thermo) {
