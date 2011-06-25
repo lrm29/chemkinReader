66d52735d8d50a6682c4c0a8765d3abe237a39cd
Laurence R. McGlashan <lrm29@cam.ac.uk>
2011-06-25 11:33:55 +0100

    Use boost to convert to caps

--- src/stringFunctions.cpp
+++ src/stringFunctions.cpp
@@ -7,6 +7,8 @@
 
 #include "stringFunctions.h"
 #include "boost/regex.hpp"
+#include "boost/algorithm/string.hpp"
+#include <string>
 
 using namespace std;
 using namespace boost;
@@ -54,44 +56,9 @@ IO::replaceComments(string stringToReplace)
 }
 
 string
-IO::convertToCaps(const string &str)
+IO::convertToCaps(const string& str)
 {
-    string::const_iterator i;
-    string caps;
-
-    for (i=str.begin(); i!=str.end(); i++) {
-        switch (*i) {
-            case 'a': caps.append("A"); break;
-            case 'b': caps.append("B"); break;
-            case 'c': caps.append("C"); break;
-            case 'd': caps.append("D"); break;
-            case 'e': caps.append("E"); break;
-            case 'f': caps.append("F"); break;
-            case 'g': caps.append("G"); break;
-            case 'h': caps.append("H"); break;
-            case 'i': caps.append("I"); break;
-            case 'j': caps.append("J"); break;
-            case 'k': caps.append("K"); break;
-            case 'l': caps.append("L"); break;
-            case 'm': caps.append("M"); break;
-            case 'n': caps.append("N"); break;
-            case 'o': caps.append("O"); break;
-            case 'p': caps.append("P"); break;
-            case 'q': caps.append("Q"); break;
-            case 'r': caps.append("R"); break;
-            case 's': caps.append("S"); break;
-            case 't': caps.append("T"); break;
-            case 'u': caps.append("U"); break;
-            case 'v': caps.append("V"); break;
-            case 'w': caps.append("W"); break;
-            case 'x': caps.append("X"); break;
-            case 'y': caps.append("Y"); break;
-            case 'z': caps.append("Z"); break;
-            default: caps.append(i,i+1); break;
-        }
-    }
-    return caps;
-
+    return boost::to_upper_copy(str);
 }
 
 string
